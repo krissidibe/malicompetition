d93f39d8505af00168ca2618fc9892f3462aa828
krissidibe <assowlove@gmail.com>
2023-05-22 18:27:13 +0000

    fix error environ

--- src/app/(admin)/admin/competitions/CompetitionList.tsx
+++ src/app/(admin)/admin/competitions/CompetitionList.tsx
@@ -4,30 +4,30 @@ import CompetitionCardComponent from "@/components/CompetitionCardComponent";
 import Link from "next/link";
 import { revalidateTag } from 'next/cache'
 import { Competition } from "../../../../../typings";
-/* const getAllDatas = async () => {
+const getAllDatas = async () => {
   const res = await fetch(`${process.env.BASE_URL}/api/user/competition/`,{next:{revalidate:5}});
   const datas:Competition[] = await res.json()
   
   
   return datas;
-}; */
+};
 
 export default async function CompetitionList() {
  
- // const datas = await  getAllDatas()
+  const datas = await  getAllDatas()
  
   return (
     <div className="grid items-center w-full sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 md:flex-row">
-      {/* @ts-ignore */}
-      {/*  {datas.map((data) => (
+       {datas.map((data) => (
       <Link  key={data.id} href="competitions/1">
+              {/* @ts-ignore */}
         <CompetitionCardComponent
           key={data.id}
           data={data}
           imageUrl={`https://picsum.photos/300/200?random=${data.id}`}
         />
       </Link>
-    ))}   */}
+    ))}  
     </div>
   );
 }
