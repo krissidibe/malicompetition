b9728d1c797384cdf7b24ef5c2055c68b0f4cdae
krissidibe <assowlove@gmail.com>
2023-05-22 23:40:01 +0000

    fix error API Test20

--- src/app/(user)/user/competitions/CompetitionList.tsx
+++ src/app/(user)/user/competitions/CompetitionList.tsx
@@ -4,22 +4,23 @@ import CompetitionCardComponent from "@/components/CompetitionCardComponent";
 import Link from "next/link";
 import { revalidateTag } from 'next/cache'
 import { Competition } from "../../../../../typings";
-/* const getAllCompetitions = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/api/competition`,{next:{revalidate:5}});
+const getAllCompetitions = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/api/user`,{next:{revalidate:5}});
  const Competitions:any[] = await res.json()
  
  
   
   return Competitions;
-}; */
+};
 
 export default async function CompetitionList() {
  
-  //const Competitions = await getAllCompetitions()
+  const Competitions = await getAllCompetitions()
   
    {/* @ts-ignore */}
   return (
-   <> 
+   <>
+ 
    {/*  <div className="grid items-center w-full sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 md:flex-row">
        {datas.map((data) => (
       <Link  key={data.id} href="competitions/1">
