a89628baaa4265233ccb7015d583fc1bd5c9c44d
krissidibe <assowlove@gmail.com>
2023-05-22 23:23:15 +0000

    fix error API Test15

--- src/app/api/competition/index.ts
+++ src/app/api/competition/index.ts
@@ -5,35 +5,26 @@ import { NextApiRequest ,NextApiResponse} from "next";
  
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-  if (req.method === 'GET') {
-    try {
-      const datasPrisma = await prisma.competition.findMany({
-        where: {
-          statut: {
-            in: ["1", "2"],
-          },
+  try {
+    const datasPrisma = await prisma.competition.findMany({
+      where: {
+        statut: {
+          in: ["1", "2"],
         },
-        orderBy: [
-          {
-            createdAt: "desc",
-          },
-          {
-            title: "desc",
-          },
-        ],
-      });
-      //  console.log(searchParams.get("name"));
-      res.status(200).json({ name: 'John Doe' }); 
-      
-    } catch (error) {
-      res.status(500).json({ message: 'Error' }); 
-    }
-  } 
- else if (req.method === 'POST') {
-    // Process a POST request
-  } 
-  
-  else {
-    // Handle any other HTTP method
+      },
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+        {
+          title: "desc",
+        },
+      ],
+    });
+    //  console.log(searchParams.get("name"));
+    res.status(200).json({ data:datasPrisma }); 
+    
+  } catch (error) {
+    res.status(500).json({ message: 'Error' }); 
   }
 }
