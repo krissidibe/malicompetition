8e8b20c6ea86df9da3d5f1620a1f57353baeb500
krissidibe <assowlove@gmail.com>
2023-05-22 23:04:21 +0000

    fix error API Test11

--- src/app/api/competition/route.ts
+++ src/app/api/competition/route.ts
@@ -22,7 +22,7 @@ export async function GET(req: NextRequest) {
       ],
     });
     //  console.log(searchParams.get("name"));
-    return new Response(JSON.stringify({datas:"data"}));
+    return new Response(JSON.stringify({datas:datasPrisma}));
   } catch (error) {
     return new Response(JSON.stringify({error:"error"}));
   }
