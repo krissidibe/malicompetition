8b91ae085fee5b0e9e3e9b6dae4f17d6de32483f
krissidibe <assowlove@gmail.com>
2023-05-26 04:04:48 +0000

    fix error Apid

--- src/app/(admin)/admin/competitions/CompetitionList.tsx
+++ src/app/(admin)/admin/competitions/CompetitionList.tsx
@@ -3,28 +3,20 @@ import { Board } from "../../../../../typings";
  import {prisma} from '../../../../utils/prisma'
 import CompetitionCardComponent from "@/components/CompetitionCardComponent";
 import Link from "next/link";
- 
-
- 
-   function getDatas() {
-    return prisma.competition.findMany({
-    orderBy:{
-     createdAt:"desc"
-    }
-   })
-  
-}
 
 const CompetitionList = async () => {
+  const datas = await prisma.competition.findMany({
+   orderBy:{
+    createdAt:"desc"
+   }
+  })
   
-  
-  const datas = await getDatas();
   return (
     
     
      <div className="grid items-center w-full sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 md:flex-row">
        {datas.map((data) => (
-      <Link  passHref key={data.id} href={`/user/competitions/${data.id}`}>
+      <Link  key={data.id} href={`/user/competitions/${data.id}`}>
            {/*  @ts-ignore */}
         <CompetitionCardComponent
           key={data.id}
