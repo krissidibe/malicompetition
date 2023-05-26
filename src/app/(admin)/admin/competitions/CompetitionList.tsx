891bba1294e43ddb28cd537b37b3ab249c76c911
krissidibe <assowlove@gmail.com>
2023-05-26 13:17:56 +0000

    fix error fix last last

--- src/app/(admin)/admin/competitions/CompetitionList.tsx
+++ src/app/(admin)/admin/competitions/CompetitionList.tsx
@@ -11,23 +11,23 @@ const CompetitionList = async () => {
     createdAt:"desc"
    }
   }) */
-  const res = await fetch(`${process.env.BASE_URL}/api/hello`,{next:{revalidate:2}})
-  const datas= await res.json();
+  const res = await fetch(`${process.env.BASE_URL}/api/admin/competition`,{next:{revalidate:0}})
+  const datas: any[] = await res.json();
   return (
     
     
      <div className="grid items-center w-full sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 md:flex-row">
-      {JSON.stringify(datas)}
-    {/*    {datas.map((data) => (
+     
+       {datas.map((data) => (
       <Link  key={data.id} href={`/user/competitions/${data.id}`}>
-           @ts-ignore  
+{/*            @ts-ignore   */}
         <CompetitionCardComponent
           key={data.id}
           data={data}
           imageUrl={`https://picsum.photos/300/200?random=${data.id}`}
         />
       </Link>
-    ))}   */}
+    ))}  
     </div>
     
     
