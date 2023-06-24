bef99c31370b19a17ef95c76dc81906c46140da4
krissidibe <assowlove@gmail.com>
2023-06-24 16:16:03 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const nextConfig = {
+    output: 'export',
+  distDir: 'out',
+  images: { unoptimized: true } ,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
